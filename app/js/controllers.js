aacd1fe145e25baa4b04f5f1fbb983144ff84258
Mortimer <mortyh@gmail.com>
2014-03-03 23:51:08 +0100

    Improved myshows view with a sidebar that shows summary info for show

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,25 @@
 
 /* Controllers */
 
-angular.module('tvtrack.controllers', [])
-  .controller('MyShowsController', ['$scope', 'showsFactory', function($scope, showsFactory) {
-  	$scope.shows = showsFactory.getFollowed();
-  }])
-  .controller('MyCtrl2', [function() {
+var tvtrackControllers = angular.module('tvtrack.controllers', []);
 
-  }]);
+tvtrackControllers.controller('MyShowsController', ['$scope', 'showsFactory', function($scope, showsFactory) {
+  	$scope.shows = showsFactory.getFollowed();
+  	
+  	$scope.toggleDetailsSidebar = function (show_id) {
+  		$scope.showDetailsSidebar = show_id;
+  		$scope.seasons = [ {season_no: 1, episodes : [1, 2, 3, 4]}, {season_no: 2, episodes : [1, 2, 3, 4]} ];
+  		$scope.details = $scope.shows.filter(function(s) {
+	  		return s.id == show_id;
+	  	})[0];
+  		console.log("Show details view for show_id=" + show_id);
+  	};
+
+  	$scope.markWatched = function (show_id, episode_id) {
+  		console.log("Marked episode_id: " + episode_id + " of show_id: " + show_id + " as watched!");
+  	}
+}])
+
+tvtrackControllers.controller('MyCtrl2', [function() {
+
+}]);
