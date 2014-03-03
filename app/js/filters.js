7262f1728f69f2e5e72b5c0ce33e535c17895287
Mortimer <mortyh@gmail.com>
2014-03-03 23:50:39 +0100

    Added filter numberFixedLen:length.

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,25 @@
 
 /* Filters */
 
-angular.module('tvtrack.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
+var tvtrackFilters = angular.module('tvtrack.filters', []);
+
+tvtrackFilters.filter('interpolate', ['version', function(version) {
+  return function(text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  }
+}]);
+
+tvtrackFilters.filter('numberFixedLen', function () {
+  return function (n, len) {
+    var num = parseInt(n, 10);
+    len = parseInt(len, 10);
+    if (isNaN(num) || isNaN(len)) {
+      return n;
     }
-  }]);
+    num = ''+num;
+    while (num.length < len) {
+    	num = '0'+num;
+    }
+    return num;
+  };
+});
